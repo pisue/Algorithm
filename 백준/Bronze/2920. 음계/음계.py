0fac57ae13fcb756963cbde6e8a091e8c81cbcd6
유화평 <96754835+pisue@users.noreply.github.com>
2024-12-16 13:17:38 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -4,20 +4,11 @@
 2. 이 값을 오름차순과 내림차순으로 변수에 담고
 3. 입력받은 값의 list와 비교한다
 """
-# 입력 받기
 numbers = list(map(int, input().split()))
 
-# 오름차순 정렬
-ascending = sorted(numbers)
-
-# 내림차순 정렬
-descending = sorted(numbers, reverse=True)
-
-# 둘다 아니면 mixed
-
-if ascending == numbers:
+if sorted(numbers) == numbers:
     print("ascending")
-elif descending == numbers:
+elif sorted(numbers, reverse=True) == numbers:
     print("descending")
 else:
     print("mixed")
